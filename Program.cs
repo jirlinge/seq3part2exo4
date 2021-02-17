d36c61fedd167fb4d7a31ef93ba7e0ad3a55ea20
jirlinge <julienirlinger@gmail.com>
2021-02-17 13:49:04 +0100

    version corrigé

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            const string login = "jirlinge";
+            const string psw = "coucou";
+            
+
+            Console.WriteLine("veuillez renseigner votre login");
+            string loginUs = Console.ReadLine();
+            Console.WriteLine("veuillez renseigner votre mdp");
+            string pswUs = Console.ReadLine();
+
+
+
+            switch (loginUs, pswUs)
+            {
+                case (login, psw):
+                
+                    Console.WriteLine("bienvenu a la Manu");
+                    break;
+
+              
+                   
+                default:
+                    Console.WriteLine("Mauvais identifiant ou mauvais mot de pass");
+                    break;
+            }
+
+            
+           
+
+        
+        }
+    }
+}
